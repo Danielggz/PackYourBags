89573702b539813014587176dc11b0249aad81bf
Danielggz <dggz1993@gmail.com>
2025-12-18 21:58:44 +0000

    Prototype features

---
+++ target/PackYourBags-1.0-SNAPSHOT/Views/scripts/login.js
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+$("#formLogin").submit(function(e){
+    e.preventDefault();
+    
+    window.location.href = "/views/mainMenu.html";
+});
+
+
