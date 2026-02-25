836513f903bdd267a4f84bc84f5279da5495a0a0
Danielggz <dggz1993@gmail.com>
2026-02-25 14:40:35 +0000

    New Backend using Maven Spring Boot

---
+++ PackYourBags_Backend/src/test/java/com/example/packyourbags_backend/PackYourBagsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packyourbags_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackYourBagsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
