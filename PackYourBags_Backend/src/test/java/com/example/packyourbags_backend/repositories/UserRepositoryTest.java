22942c535961ae381d27de9ed1b6510c290ea49c
Danielggz <dggz1993@gmail.com>
2026-04-09 16:57:18 +0100

    Add fix for specifying to use test db

--- PackYourBags_Backend/src/test/java/com/example/packyourbags_backend/repositories/UserRepositoryTest.java
+++ PackYourBags_Backend/src/test/java/com/example/packyourbags_backend/repositories/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.MySQLContainer;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Testcontainers
+@ActiveProfiles("test")
 class UserRepositoryTest extends AbstractMysqlTest {
 
     @Container
--- PackYourBags_Backend/src/test/java/com/example/packyourbags_backend/services/UserServiceTest.java
+++ PackYourBags_Backend/src/test/java/com/example/packyourbags_backend/services/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class UserServiceTest extends AbstractMysqlTest {
 
     @Mock
